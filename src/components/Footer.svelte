e035b9efa12da60dc4c3f404f1fae7d36733df41
ParmesanLinguin <40549567+ParmesanLinguin@users.noreply.github.com>
2023-04-22 02:09:58 -0400

    refactor: begin custom navbar impl

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,7 @@
 <script>
   import SocialTag from "./SocialTag.svelte";
-
 </script>
+
 <div class="flex flex-col justify-center items-center bg-[#09090b] pt-10 pb-10">
   <p class="font-libre-franklin text-white text-2xl font-bold mb-4">Socials</p>
   <div class="flex flex-row justify-center">
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  let customClass: string = "";
+	export { customClass as class };
+</script>
+
+<div class="{customClass} w-screen m-auto">
+  <div class="m-auto max-w-[1600px] w-screen flex flex-row justify-end items-center h-16 px-8">
+    <slot />
+  </div>
+</div>
